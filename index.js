eeaab756d64dcfd4c54b32cd9c15d543466959ac
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-27 16:18:31 +0200

    Added Sketch Functionality
    Implemented the code to color the generated grid items when a user hovers over them.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const userSelection = document.querySelectorAll('.brush');
 
 userSelection.forEach((brush) =>{
     brush.addEventListener('click', () => {
-        console.log(brush);
         container.innerHTML = "";
         const rect = brush.getBoundingClientRect();
         const squareDimensions = rect.width;
@@ -14,17 +13,20 @@ userSelection.forEach((brush) =>{
 
         let gridDimension = containerSize/squareDimensions;
         const numSquares = gridDimension*gridDimension;
-        createGridItems();
+        
+        for (let i = 0; i < numSquares; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.style.height = squareDimensionsString;
+            gridItem.style.width = squareDimensionsString;
+            container.appendChild(gridItem);
 
-        function createGridItems() {
-            for (let i = 0; i < numSquares; i++) {
-                const gridItem = document.createElement('div');
-                gridItem.classList.add('grid-item');
-                gridItem.style.height = squareDimensionsString;
-                gridItem.style.width = squareDimensionsString;
-                container.appendChild(gridItem);
-            };
+            gridItem.addEventListener('mouseenter', ()=>{
+                gridItem.style.backgroundColor = "rgba(95, 105, 131, 1)";
+            })
         };
+
+
     })
 });
 
