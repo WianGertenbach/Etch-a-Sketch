22ac986a7b9de898d8af19dfec869751181bda6f
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-27 16:08:43 +0200

    Added Interactive Brush Functionality
    Added event listeners to each brush to allow the grid to regenerate for each size. Also implemented the reset button.

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const container = document.querySelector('.grid-container');
 
 const containerSize = 400;
 
-const userSelection = document.querySelector('.b4');
+const userSelection = document.querySelectorAll('.brush');
 
-const rect = userSelection.getBoundingClientRect();
+userSelection.forEach((brush) =>{
+    brush.addEventListener('click', () => {
+        console.log(brush);
+        container.innerHTML = "";
+        const rect = brush.getBoundingClientRect();
+        const squareDimensions = rect.width;
+        const squareDimensionsString = squareDimensions+"px";
 
-const squareDimensions = rect.width;
-const squareDimensionsString = squareDimensions+"px";
+        let gridDimension = containerSize/squareDimensions;
+        const numSquares = gridDimension*gridDimension;
+        createGridItems();
 
-let gridDimension = containerSize/squareDimensions;
-const numSquares = gridDimension*gridDimension;
+        const reset = document.querySelector('button');
+        reset.addEventListener('click', ()=> {
+            container.innerHTML = "";
+        });
 
-function createGridItems() {
-    for (let i = 0; i < numSquares; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.style.height = squareDimensionsString;
-        gridItem.style.width = squareDimensionsString;
-        container.appendChild(gridItem);
-    };
-};
+        function createGridItems() {
+            for (let i = 0; i < numSquares; i++) {
+                const gridItem = document.createElement('div');
+                gridItem.classList.add('grid-item');
+                gridItem.style.height = squareDimensionsString;
+                gridItem.style.width = squareDimensionsString;
+                container.appendChild(gridItem);
+            };
+        };
+    })
+});
 
-createGridItems();
