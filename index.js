3d392032abc4a82a423a56f7566b91f17816b937
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2024-12-27 15:16:02 +0200

    Added Grid Generation Function
    Function to add grid items based on user input implemented.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const container = document.querySelector('.grid-container');
 
-const containerSize = container.getBoundingClientRect();
-const size = containerSize.width;
+const containerSize = 400;
 
-console.log("The container size: "+size);
+let userInput = prompt("Enter the size of the grid: ");
+let gridDimension = Number(userInput);
+
+const squareDimensions = containerSize/gridDimension;
+const squareDimensionsString = squareDimensions+"px"
+const numSquares = gridDimension*gridDimension;
 
 function createGridItems() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < numSquares; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
+        gridItem.style.height = squareDimensionsString;
+        gridItem.style.width = squareDimensionsString;
         container.appendChild(gridItem);
-
     };
 };
 
